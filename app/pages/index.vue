df93c5260317b32a62f32a7f79e8f7912e2c6350
taninpong <thaninpong@perfenterprise.com>
2024-11-05 19:20:27 +0700

    update ui

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,29 +1,35 @@
 <template>
   <div>
     <UPage>
-      <UPageBody>
-        <ULandingSection :ui="{ wrapper: 'py-0 sm:py-0' }" style="background-image: image-set(url(https://failfast.blob.core.windows.net/upload/Artboard3.png
-        ));">
+      <UPageBody :ui="{ wrapper: 'mt-0 pb-0' }">
+        <ULandingSection
+          class="bg-contain bg-no-repeat bg-center bg-gray-200 bg-[url('https://failfast.blob.core.windows.net/upload/webmana/Heropattern3.png')]"
+          :ui="{ wrapper: 'py-0 sm:py-0', container: 'gap-0 sm:gap-y-0' }">
           <UPageHero title="Simpler Faster Safer"
-            :ui="{ wrapper: 'py-0 sm:py-0 pt-8 sm:pt-16', title: 'lg:text-9xl', description: 'lg:text-3xl' }"
-            description="Experience seamless payments with Mana—no banks, no delays, just full control over your money."
-            align="left">
+            :ui="{ wrapper: 'py-0 sm:py-0 pt-0 sm:pt-0 -mb-0 sm:-mb-24', container: 'sm:gap-y-0', title: 'lg:text-9xl', description: 'lg:text-3xl' }"
+            description="">
             <!-- :links="[{ label: 'JOIN NOW', size: 'lg' }]" -->
             <template #links>
-              <UButton size="lg" class="bg-[#ed1c24]">JOIN NOW</UButton>
+              <label class="text-[#ed1c24] lg:text-3xl"></label>
             </template>
-            <img src="https://failfast.blob.core.windows.net/upload/blank_smartphone.png"
-              class="lg:h-[100%]    dark:ring-gray-700 justify-self-center " />
+            <template #description>
+              <p class="lg:text-3xl">Experience seamless payments with <br>Mana—no banks, no delays, just
+                full control over
+                your money.</p>
+              <!-- <UButton size="lg" class="bg-[#ed1c24]">JOIN NOW</UButton> -->
+            </template>
+            <img src="https://failfast.blob.core.windows.net/upload/webmana/blank_smartphone.png"
+              class="lg:h-3/4  dark:ring-gray-700 justify-self-center " />
           </UPageHero>
         </ULandingSection>
 
-        <ULandingSection :ui="{ wrapper: 'p-9 ' }">
+        <ULandingSection :ui="{ wrapper: 'p-0 sm:py-16' }">
           <video width="100%" height="480" controls>
-            <source src="https://failfast.blob.core.windows.net/upload/VDO/manavdo.mp4" type="video/mp4">
+            <source src="https://failfast.blob.core.windows.net/upload/webmana/vdo/manavdo.mp4" type="video/mp4">
           </video>
         </ULandingSection>
 
-        <ULandingSection title="Why mana"
+        <ULandingSection :ui="{ wrapper: 'p-0 sm:py-16' }" title="Why mana"
           description="Having a solution isn't enough—it needs to be efficient, reliable, and adaptable. Mana offers a flexible platform with unique features, ensuring every transaction is fast, secure, and perfectly tailored to your needs.">
           <UPageGrid id="features" class="scroll-mt-[calc(var(--header-height)+140px+128px+96px)]">
             <ULandingCard v-for="(item, index) in itemFeature" :key="index" v-bind="item"
@@ -43,7 +49,6 @@
           description="Discover how Mana helps you manage payments with ease and flexibility. Here are the most common questions.">
           <ULandingFAQ id="faq" :items="itemsFaq" multiple />
         </ULandingSection>
-
       </UPageBody>
     </UPage>
   </div>
@@ -85,21 +90,19 @@ const itemFeature = ref([
 ]);
 
 const dataTestimonials = ref([{
-  quote: 'Onboarding with Mana was incredibly simple. No more tedious paperwork or compliance headaches—just a seamless connection to the platform. We were up and running in no time, and Mana took care of everything behind the scenes, allowing us to focus on what matters most."',
+  quote: 'Onboarding with Mana was incredibly simple. No more tedious paperwork or compliance headaches—just a seamless connection to the platform. We were up and running in no time, and Mana took care of everything behind the scenes, allowing us to focus on what matters most.',
   author: {
     name: 'Siriwasan Akanitthaphichat, CTO at Jergun',
-    job: 'Creator of Vue.js and Vite',
     avatar: {
-      src: 'https://ipx.nuxt.com/f_auto,s_40x40/gh_avatar/yyx990803'
+      src: 'https://failfast.blob.core.windows.net/upload/webmana/people/Siriwasan.png'
     }
   }
 }, {
   quote: 'As a third-party developer working with various business requirements, Mana\'s API truly impressed me. Its flexibility caters to different business needs, offering a wide range of commands that go beyond what other financial platforms provide. Features like real-time revenue sharing are a game-changer, making Mana the perfect solution for any client I work with.',
   author: {
     name: 'Sakul Jaruthanaset, Independent Software Developer',
-    job: 'Co-founder and CEO of Vercel',
     avatar: {
-      src: 'https://ipx.nuxt.com/f_auto,s_40x40/gh_avatar/rauchg'
+      src: 'https://failfast.blob.core.windows.net/upload/webmana/people/Sakul.png'
     }
   }
 }
@@ -107,9 +110,8 @@ const dataTestimonials = ref([{
   quote: 'With Mana, I no longer have to worry about constantly checking my employees’ work or the money coming in each day. The platform’s automation ensures that everything is handled seamlessly, and if there’s ever a mistake, the transaction won’t even trigger. It’s a relief knowing that everything is managed securely and automatically, giving me more time to focus on growing my business.',
   author: {
     name: 'Tanyanan Kiatsuranon, Business Owner',
-    job: 'Chief Engineer of Chrome',
     avatar: {
-      src: 'https://ipx.nuxt.com/f_auto,s_40x40/gh_avatar/addyosmani'
+      src: 'https://failfast.blob.core.windows.net/upload/webmana/people/Tanyanan.png'
     }
   }
 }
@@ -117,45 +119,40 @@ const dataTestimonials = ref([{
   quote: 'Before using Mana, settling payments between restaurants and riders was a constant hassle. Now, with Mana’s conditional transactions, everything is done automatically. I don’t have to manually handle any of it, which has been such a time-saver and makes my job so much easier.',
   author: {
     name: 'Orawan Permsree, Food Delivery Administrator at Solar Delivery',
-    job: 'Director of Engineering, Google',
     avatar: {
-      src: 'https://ipx.nuxt.com/f_auto,s_40x40/gh_avatar/sdras'
+      src: 'https://failfast.blob.core.windows.net/upload/webmana/people/Orawan.png'
     }
   }
 }, {
-  quote: 'With traditional bank services, I could only process payroll once a month, and it came with fees. But with Mana, I have the flexibility to manage payroll anytime. I’m also able to automatically track and collect any payroll advances made by employees—all with just a one-time setup. It’s a game-changer for HR management."',
+  quote: 'With traditional bank services, I could only process payroll once a month, and it came with fees. But with Mana, I have the flexibility to manage payroll anytime. I’m also able to automatically track and collect any payroll advances made by employees—all with just a one-time setup. It’s a game-changer for HR management.',
   author: {
     name: 'Tippawan Sayoomporn, HR at Prompiman Hotel',
-    job: 'Co-founder of Storyblok',
     avatar: {
-      src: 'https://ipx.nuxt.com/f_auto,s_40x40/gh_avatar/DominikAngerer'
+      src: 'https://failfast.blob.core.windows.net/upload/webmana/people/Tippawan.png'
     }
   }
 }, {
-  quote: 'Running a night pub means constantly managing promotions, which used to be a headache. Thanks to Mana’s coupon features, I just set the rules for each promotion, and everything runs automatically—without any errors! It’s made managing promotions so much easier and stress-free."',
+  quote: 'Running a night pub means constantly managing promotions, which used to be a headache. Thanks to Mana’s coupon features, I just set the rules for each promotion, and everything runs automatically—without any errors! It’s made managing promotions so much easier and stress-free.',
   author: {
     name: 'Chanawee Jittumnan, Manager at iBAR',
-    job: 'Sernior Frontend Engineer at GitLab',
     avatar: {
-      src: 'https://ipx.nuxt.com/f_auto,s_40x40/gh_avatar/svedova'
+      src: 'https://failfast.blob.core.windows.net/upload/webmana/people/chanawee.png'
     }
   }
 }, {
   quote: 'Mana isn’t just about payments—it comes with a full restaurant management system, including POS and online menu integration. Now, I can handle all aspects of daily operations with ease, using the same tools as big chains but at a much lower cost. It’s made running the restaurant so much smoother.',
   author: {
     name: 'Charinrat Jantawee, Manager at SIT',
-    job: 'Sernior Frontend Engineer at GitLab',
     avatar: {
-      src: 'https://ipx.nuxt.com/f_auto,s_40x40/gh_avatar/svedova'
+      src: 'https://failfast.blob.core.windows.net/upload/webmana/people/Charinrat.png'
     }
   }
 }, {
-  quote: 'Nothing beats getting paid in real-time. - (Ask Whan), Freelancer Rider',
+  quote: 'Nothing beats getting paid in real-time.',
   author: {
-    name: 'Savas Vedova',
-    job: 'Sernior Frontend Engineer at GitLab',
+    name: 'Niphon Phanphakdee, Freelancer Rider',
     avatar: {
-      src: 'https://ipx.nuxt.com/f_auto,s_40x40/gh_avatar/svedova'
+      src: 'https://failfast.blob.core.windows.net/upload/webmana/people/FreelancerRider.png'
     }
   }
 }
@@ -197,3 +194,11 @@ useSeoMeta({
 })
 
 </script>
+
+<!-- <style>
+video[poster] {
+  width: 100%;
+  height: 480px;
+  object-fit: contain;
+}
+</style> -->
