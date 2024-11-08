4e62f9b9f3d6b7e9febad0b4f53d93fb65ed5480
taninpong <thaninpong@perfenterprise.com>
2024-11-08 16:35:25 +0700

    change logo website

--- app/app.vue
+++ app/app.vue
@@ -4,7 +4,7 @@ useHead({
     { name: 'viewport', content: 'width=device-width, initial-scale=1' }
   ],
   link: [
-    { rel: 'icon', href: '/favicon.ico' }
+    { rel: 'icon', href: 'https://failfast.blob.core.windows.net/upload/Logo_manaForm.png' },
   ],
   htmlAttrs: {
     lang: 'en'
