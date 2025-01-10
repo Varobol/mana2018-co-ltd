2acfea84be892cad1ed31d8d59ec7d1fb58eeec4
taninpong <thaninpong@perfenterprise.com>
2025-01-10 14:56:12 +0700

    update page app

--- app/app.vue
+++ app/app.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+const { data: page } = await useAsyncData('', () => queryContent('/').findOne());
+
 useHead({
   meta: [
     { name: 'viewport', content: 'width=device-width, initial-scale=1' }
@@ -12,7 +14,12 @@ useHead({
 })
 
 useSeoMeta({
-  ogImage: 'https://failfast.blob.core.windows.net/upload/webmana/webmana.png',
+  title: page.value.title,
+  ogTitle: page.value.title,
+  description: "What is mana??",
+  ogDescription: "What is mana???",
+  ogImage: "https://failfast.blob.core.windows.net/upload/webmana/Logo_manaForm.png",
+  ogImageUrl: "https://failfast.blob.core.windows.net/upload/webmana/Logo_manaForm.png",
 })
 </script>
 
