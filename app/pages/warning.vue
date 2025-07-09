beb8d46c925e7853600d655a4d8dd96f77e53536
taninpong <thaninpong@perfenterprise.com>
2025-07-09 16:54:58 +0700

    update spacing image

--- app/pages/warning.vue
+++ app/pages/warning.vue
@@ -4,19 +4,23 @@
       <UPageBody :ui="{ wrapper: 'mt-0 pb-0' }">
         <img src="https://failfast.blob.core.windows.net/upload/webmana/warning/anti-corruption01.png" />
         <p class="p-6 text-3xl font-bold  text-center">รู้ให้ทันภัยการเงิน</p>
-        <img class="px-64" src="https://failfast.blob.core.windows.net/upload/webmana/warning/anti-corruption02.png" />
+        <img class="lg:px-64  px-6"
+          src="https://failfast.blob.core.windows.net/upload/webmana/warning/anti-corruption02.png" />
         <p class="p-6 text-3xl font-bold  text-center">อย่าเชื่อ อย่าโอน</p>
-        <img class="px-64" src="https://failfast.blob.core.windows.net/upload/webmana/warning/anti-corruption03.png" />
+        <img class="lg:px-64  px-6"
+          src="https://failfast.blob.core.windows.net/upload/webmana/warning/anti-corruption03.png" />
         <p class="p-6 text-3xl font-bold  text-center">ยกระดับการจัดการบัญชีม้า</p>
-        <img class="px-64" src="https://failfast.blob.core.windows.net/upload/webmana/warning/anti-corruption04.png" />
+        <img class="lg:px-64  px-6"
+          src="https://failfast.blob.core.windows.net/upload/webmana/warning/anti-corruption04.png" />
         <p class="p-6 text-3xl font-bold  text-center">แจ้งเหตุทันที</p>
-        <img class="px-64" src="https://failfast.blob.core.windows.net/upload/webmana/warning/anti-corruption05.png" />
+        <img class="lg:px-64  px-6"
+          src="https://failfast.blob.core.windows.net/upload/webmana/warning/anti-corruption05.png" />
         <p class="p-6 text-3xl font-bold  text-center">แฉกลโกง มิจฉาชีพ</p>
-        <img class="pb-6 px-64"
+        <img class="pb-6 lg:px-64  px-6"
           src="https://failfast.blob.core.windows.net/upload/webmana/warning/anti-corruption06.png" />
-        <img class="pb-6 px-64"
+        <img class="pb-6 lg:px-64  px-6"
           src="https://failfast.blob.core.windows.net/upload/webmana/warning/anti-corruption07.png" />
-        <img class="pb-6 px-64"
+        <img class="pb-6 lg:px-64  px-6"
           src="https://failfast.blob.core.windows.net/upload/webmana/warning/anti-corruption08.png" />
       </UPageBody>
     </UPage>
